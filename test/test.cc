cad583010bb2ad1b54ac94f9f15e609b330c944a
Felix Gündling <felix.guendling@gmail.com>
2024-08-03 21:15:52 +0200

    more test cases

--- test/test.cc
+++ test/test.cc
@@ -13,7 +13,19 @@ TEST(a, b) {
       "Sa 08:50-13:40, 14:25-19:10; "
       "Su 08:50-13:45, 14:45-21:10");
   EXPECT_FALSE(
-      contains(r, local_minutes{local_days{2024_y / August / 3} + 8h + 49min}));
+      contains(r, local_minutes{local_days{2024_y / August / 4} + 8h + 49min}));
   EXPECT_TRUE(
-      contains(r, local_minutes{local_days{2024_y / August / 3} + 8h + 50min}));
+      contains(r, local_minutes{local_days{2024_y / August / 4} + 8h + 50min}));
+  EXPECT_FALSE(contains(
+      r, local_minutes{local_days{2024_y / August / 3} + 14h + 24min}));
+  EXPECT_TRUE(contains(
+      r, local_minutes{local_days{2024_y / August / 3} + 14h + 25min}));
+  EXPECT_TRUE(contains(
+      r, local_minutes{local_days{2024_y / August / 2} + 12h + 46min}));
+  EXPECT_FALSE(contains(
+      r, local_minutes{local_days{2024_y / August / 2} + 12h + 44min}));
+  EXPECT_FALSE(contains(
+      r, local_minutes{local_days{2024_y / August / 2} + 18h + 11min}));
+  EXPECT_FALSE(
+      contains(r, local_minutes{local_days{2024_y / August / 2} + 7h + 30min}));
 }
